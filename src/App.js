c33273a3a91aa4fce00d8ad3b266ce570d54a890
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-06-27 19:08:57 +0100

    form section created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Navbar } from "./components/Navbar";
+import Navbar from "./components/Navbar";
+import Create from "./components/Create";
+import Blogs from "./components/Blogs";
 
 function App() {
   return (
-    <div className="container">
-      <Routes>
-        <Route path="/" element={ <Navbar /> }/>
-        <Route path="create" element={ <Navbar/> }/>
-      </Routes>
-    </div>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Blogs />} exact />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+        {/* <Create /> */}
+      </div>
   );
 }
 
