6331f0017919263c0b6eb83c181573d8c6e5c4a7
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-06-28 18:00:06 +0100

    creatPost func, API call, rm Data.js & readme

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,43 +1,80 @@
-const Create = () => {
+import { useState } from "react";
+import Axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+export const BASE_URL = 'https://blogpostapi1.herokuapp.com/'
+
+const CreatePost = () => {
+  const [newBlog, setNewBlog] = useState({ title: "", content: "", name: "" })
+  let navigate = useNavigate();
+
+  const getBlogDetails = ((event) =>{
+    setNewBlog(current => ({...current, [event.target.name]: event.target.value}))
+  })
+
+  // sumbit blog post func
+  const submitBlogPost = (async() =>{
+    try {
+      const result = await Axios.post(BASE_URL, newBlog)
+      if(result?.status === 200){
+        return navigate("/", {replace: true});
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
   return (
     <div className="form-section">
       <h3 className="form-heading">Create Blog</h3>
 
       <div className="form-container">
-        <form action="" method="POST">
+        <form>
           <div className="form-control">
-            <label for="title">Blog Title</label>
+            <label htmlFor="title">Blog Title</label>
             <input
               className="form-input"
               type="text"
+              id="title"
+              name="title"
+              value={newBlog.title}
               placeholder="title"
-              required
+              onChange={getBlogDetails}
+            
             ></input>
           </div>
           <div className="form-control">
-            <label for="content">Blog Content</label>
+            <label htmlFor="content">Blog Content</label>
             <textarea
               className="form-input-content"
               type="text"
+              id="content"
+              name="content"
+              value={newBlog.content}
               placeholder="content"
-              required
+              onChange={getBlogDetails}
+            
             ></textarea>
           </div>
           <div className="form-control">
-            <label for="author">Author</label>
+            <label htmlFor="author">Author</label>
             <input
               className="form-input"
               type="text"
+              id="author"
+              name="name"
+              value={newBlog.name}
               placeholder="name"
-              required
+              onChange={getBlogDetails}
+          
             ></input>
           </div>
 
-          <input className="submit-btn" type="submit" required></input>
+          <input className="submit-btn" type="submit" onClick={submitBlogPost} required></input>
         </form>
       </div>
     </div>
   );
 };
 
-export default Create;
+export default CreatePost;
